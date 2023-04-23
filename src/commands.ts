7ce5521d62f914f469c9ef0948cd03e170792992
Thierry Parmentelat <thierry.parmentelat@inria.fr>
2023-04-23 15:42:12 +0200

    the list of applicable languages is customizable as the jupytext.languages setting

--- src/commands.ts
+++ src/commands.ts
@@ -1,24 +1,23 @@
-import { commands, Uri, window } from 'vscode';
+import { commands, Uri, window, workspace } from 'vscode';
 import { JupyterNotebookView, jupytextScheme } from './constants';
 import { convertToNotebook } from './conversion';
 
+// see the default values of jupytext.languages in package.json
+// this should reflect the set of available languages
+// as defined in python-libs/jupytext/languages.py
+// >>> from jupytext.languages import _SCRIPT_EXTENSIONS
+// >>> langs = sorted(list({ record['language'] for ext, record in _SCRIPT_EXTENSIONS.items()}))
+// >>> "\n".join(langs)
 export function initialize() {
+    const config = workspace.getConfiguration('jupytext');
+    // reassemble and remove empty lines if any
+    console.log(config);
+    const langIds = config.languages.split("\n").filter((x:any) => x);
     commands.executeCommand(
         'setContext',
         'vscode-jupytext.resourceLangIds',
-        [
-            'csharp',
-            'fsharp',
-            'java',
-            'javascript',
-            'julia',
-            'markdown',
-            'powershell',
-            'python',
-            'r',
-            'rust',
-            'typescript'
-        ]);
+        langIds,
+    );
 
     commands.registerCommand('jupyter.openAsPairedNotebook', async (uri?: Uri) => {
         uri = uri || window.activeTextEditor?.document.uri ;
--- src/languages.ts
+++ src/languages.ts
@@ -1,34 +1,37 @@
 import { Uri } from "vscode"
 
-export const supportedFileExtensions = {
-    ".py": {"language": "python", "comment": "#"},
-    ".coco": {"language": "coconut", "comment": "#"},
-    ".R": {"language": "R", "comment": "#"},
-    ".r": {"language": "R", "comment": "#"},
-    ".jl": {"language": "julia", "comment": "#"},
-    ".cpp": {"language": "c++", "comment": "//"},
-    ".ss": {"language": "scheme", "comment": ";;"},
-    ".clj": {"language": "clojure", "comment": ";;"},
-    ".scm": {"language": "scheme", "comment": ";;"},
-    ".sh": {"language": "bash", "comment": "#"},
-    ".ps1": {"language": "powershell", "comment": "#"},
-    ".q": {"language": "q", "comment": "/"},
-    ".m": {"language": "matlab", "comment": "%"},
-    ".pro": {"language": "idl", "comment": ";"},
-    ".js": {"language": "javascript", "comment": "//"},
-    ".ts": {"language": "typescript", "comment": "//"},
-    ".scala": {"language": "scala", "comment": "//"},
-    ".rs": {"language": "rust", "comment": "//"},
-    ".robot": {"language": "robotframework", "comment": "#"},
-    ".resource": {"language": "robotframework", "comment": "#"},
-    ".cs": {"language": "csharp", "comment": "//"},
-    ".fsx": {"language": "fsharp", "comment": "//"},
-    ".fs": {"language": "fsharp", "comment": "//"},
-    ".sos": {"language": "sos", "comment": "#"},
-    ".java": {"language": "java", "comment": "//"},
-    ".groovy": {"language": "groovy", "comment": "//"},
-    ".sage": {"language": "sage", "comment": "#"},
-}
+// NOTE
+// this is actually not used
+//
+// export const supportedFileExtensions = {
+//     ".py": {"language": "python", "comment": "#"},
+//     ".coco": {"language": "coconut", "comment": "#"},
+//     ".R": {"language": "R", "comment": "#"},
+//     ".r": {"language": "R", "comment": "#"},
+//     ".jl": {"language": "julia", "comment": "#"},
+//     ".cpp": {"language": "c++", "comment": "//"},
+//     ".ss": {"language": "scheme", "comment": ";;"},
+//     ".clj": {"language": "clojure", "comment": ";;"},
+//     ".scm": {"language": "scheme", "comment": ";;"},
+//     ".sh": {"language": "bash", "comment": "#"},
+//     ".ps1": {"language": "powershell", "comment": "#"},
+//     ".q": {"language": "q", "comment": "/"},
+//     ".m": {"language": "matlab", "comment": "%"},
+//     ".pro": {"language": "idl", "comment": ";"},
+//     ".js": {"language": "javascript", "comment": "//"},
+//     ".ts": {"language": "typescript", "comment": "//"},
+//     ".scala": {"language": "scala", "comment": "//"},
+//     ".rs": {"language": "rust", "comment": "//"},
+//     ".robot": {"language": "robotframework", "comment": "#"},
+//     ".resource": {"language": "robotframework", "comment": "#"},
+//     ".cs": {"language": "csharp", "comment": "//"},
+//     ".fsx": {"language": "fsharp", "comment": "//"},
+//     ".fs": {"language": "fsharp", "comment": "//"},
+//     ".sos": {"language": "sos", "comment": "#"},
+//     ".java": {"language": "java", "comment": "//"},
+//     ".groovy": {"language": "groovy", "comment": "//"},
+//     ".sage": {"language": "sage", "comment": "#"},
+// }
 
 export function isConversionSupportedWithoutPython(uri: Uri){
     if (uri.fsPath.toLowerCase().endsWith('.py')){
